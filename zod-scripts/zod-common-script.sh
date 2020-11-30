f07d4705731b60bbfc04100fd7f86c68f5835803
infamousSs <locdaohuuit@outlook.com>
2020-11-30 01:56:06 -0800

    #1 Initial project base

---
+++ zod-scripts/zod-common-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+function createMavenProject() {
+    mvn -B archetype:generate                
+        -DarchetypeGroupId=com.infamous         
+        -DarchetypeArtifactId=zod-maven-app     
+        -DarchetypeVersion=1.0.0                
+        -DgroupId=$1                           
+        -DartifactId=$2                        
+        -Dversion=$3                            
+}
+
+function mvnBuild() {
+    echo "Excuting mvn clean install $@"
+    mvn clean install $@
+}
